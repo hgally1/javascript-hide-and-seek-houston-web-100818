dc41f7bd8487ae0ab2f4d306726213bd6b523e5e
Heidi Gally <heidigally@gmail.com>
2020-03-19 16:40:40 -0500

    javascript-hide-and-seek-FINISHED

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector) {
+    return document.querySelector(`${selector}`)
+  }
 
+  function nestedTarget() {
+      return document.querySelector('#nested .target')
+  }
+function increaseRankBy(n){
+    let lis = document.querySelectorAll('ul.ranked-list')
+    let num = []
+    for (let i = 0; i < lis.length; i++) {
+        let lisNum = lis[i].querySelectorAll('li')
+        for (let j = 0; j < lisNum.length; j++){
+            num.push(lisNum[j])
+        }
+    }
+    num.forEach(function(num) {
+        num.innerHTML = parseInt(num.innerHTML) + n
+    })
+}
+
+function deepestChild(){
+    const grand = document.getElementById('grand-node')
+    return grand.querySelector('div div div div div')
+}
